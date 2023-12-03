c18725a7052cd928aabc869d0e61e5d6a419e50b
astapov <dmytroastapow@gmail.com>
2023-12-03 19:16:42 +0200

    share LATEST_MSG_IDX_BY_SENDER  global

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import socketserver
 
 
 MSG_LOG = []
+LATEST_MSG_IDX_BY_SENDER = {}
 
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
@@ -16,19 +17,19 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
         global MSG_LOG
-        self.latest_msg_idx_by_sender = getattr(self, "latest_msg_idx_by_sender", {})
+        global LATEST_MSG_IDX_BY_SENDER
         # self.request is the TCP socket connected to the client
         latest_message = pickle.loads(self.request.recv(1024).strip())
         sender = latest_message["sender"]
         MSG_LOG.append(latest_message)
-        current_counter = self.latest_msg_idx_by_sender.get(sender, 0)
-        self.latest_msg_idx_by_sender[sender] = current_counter + 1
+        current_counter = LATEST_MSG_IDX_BY_SENDER.get(sender, 0)
+        LATEST_MSG_IDX_BY_SENDER[sender] = current_counter + 1
         print("{} wrote: {}".format(sender, latest_message["text"]))
 
         # just send back the same data, but upper-cased
 
         messages = pickle.dumps(
-            MSG_LOG[self.latest_msg_idx_by_sender[sender] - 1:]
+            MSG_LOG[LATEST_MSG_IDX_BY_SENDER[sender] - 1:]
         )
         self.request.sendall(messages)
 
