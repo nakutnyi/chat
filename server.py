f19115f9a23018d79c161049fe0ca74c516cc8f2
rnakutnyi <nakutnyi@gmail.com>
2023-12-05 17:53:56 +0200

    Add Logging for an Error

--- server.py
+++ server.py
@@ -26,7 +26,14 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
         global MSG_COUNTER
         global LATEST_MSG_IDX_BY_SENDER
 
-        latest_message = pickle.loads(self.request.recv(1024))
+        try:
+            received = self.request.recv(1024)
+            latest_message = pickle.loads(received)
+        except EOFError:
+            latest_message = {
+                "sender": "system", "is_service": True, "msg": "control_flow"
+            }
+            print(received)
         sender = latest_message["sender"]
         if latest_message["is_service"] and latest_message["msg"] == "get_update":
             messages = []
